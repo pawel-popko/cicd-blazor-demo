119b2b2a74c7ba878fd590768b966e759b14e7ef
SILVER15 <pawel.popko@gmail.com>
2026-01-15 11:26:52 +0100

    Add JsonPropertyName attributes for safe deserialization

--- src/CicdBlazorDemo.Web/Info/BuildInfo.cs
+++ src/CicdBlazorDemo.Web/Info/BuildInfo.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace CicdBlazorDemo.Web.Info;
 
 public sealed class BuildInfo
 {
+    [JsonPropertyName("version")]
     public string Version { get; set; } = "unknown";
+    [JsonPropertyName("commit")]
     public string Commit { get; set; } = "unknown";
+    [JsonPropertyName("buildTimeUtc")]
     public string BuildTimeUtc { get; set; } = "unknown";
 }
